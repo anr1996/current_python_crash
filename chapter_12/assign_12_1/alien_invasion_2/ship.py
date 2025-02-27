2e99a91b92dc354ab93f04f10ca2d4e0bc2de4d7
anr1996 <anrich250@gmail.com>
2025-02-27 09:10:59 -0800

    python project update 2-27-22, 9am

---
+++ chapter_12/assign_12_1/alien_invasion_2/ship.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship."""
+    
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        
+        # load the ship image and get its rect.
+        self.image = pygame.image.load('/Users/adrianrich/Documents/python_crash_course_current/'
+                                       'chapter_12/assign_12_1/alien_invasion_2/images/alien.png')
+         # Resize the image (Change the numbers to adjust size)
+        self.image = pygame.transform.scale(self.image, (80, 80))  
+        self.rect = self.image.get_rect()
+        
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+        
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+        
+    
